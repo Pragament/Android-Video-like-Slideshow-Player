db6f8b406eb912022f51a3520eab88be28e563b8
priyanshu-shinde26 <priyanshu1983s@gmail.com>
2026-01-24 21:46:07 +0530

    fix:Modernize build for SDK 35 and fix app crashes and  resolve build issues and dependency conflicts

--- app/src/main/java/com/technikh/evideos/activities/SlideshowSplashScreen.java
+++ app/src/main/java/com/technikh/evideos/activities/SlideshowSplashScreen.java
@@ -1,14 +1,9 @@
 package com.technikh.evideos.activities;
 
-/*
- * Copyright (c) 2019. Nikhil Dubbaka from TechNikh.com under GNU AFFERO GENERAL PUBLIC LICENSE
- * Copyright and license notices must be preserved.
- * When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available.
- */
-
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,19 +15,18 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
-//import com.bumptech.glide.request.target.CustomTarget;
-import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.target.Target;
 import com.bumptech.glide.request.transition.Transition;
 import com.danikula.videocache.HttpProxyCacheServer;
-import com.technikh.evideos.R;
-
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.android.gms.security.ProviderInstaller;
 import com.google.gson.Gson;
+import com.technikh.evideos.R;
 import com.technikh.evideos.app.MyApplication;
-import com.technikh.evideos.models.slideshow.lineMedia;
 import com.technikh.evideos.models.slideshow.SlideshowJsonModel;
+import com.technikh.evideos.models.slideshow.lineMedia;
 import com.technikh.evideos.network.SlideshowGetDataService;
 import com.technikh.evideos.network.SlideshowRetrofitInstance;
 import com.technikh.evideos.preferences.SlideshowSharedPreferences;
@@ -49,16 +43,14 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 
-
 public class SlideshowSplashScreen extends AppCompatActivity {
-    HashMap<String, Boolean> downloadStatus=new HashMap<String, Boolean>();
+    HashMap<String, Boolean> downloadStatus = new HashMap<String, Boolean>();
     private SlideshowJsonModel data;
     private ImageView imageView;
     private ImagesCache cache;
     public static int CountValue = 0;
-    public static int audioCountValue = 0;
+    private int processedCount = 0; // NEW: Track processed items (success + fail)
     HttpProxyCacheServer cacheProxy;
-    private SlideshowRetrofitInstance retrofitInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,182 +58,152 @@ public class SlideshowSplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-
         cacheProxy = MyApplication.getProxy(SlideshowSplashScreen.this);
-        //getSupportActionBar().hide();
         imageView = findViewById(R.id.Image);
-        try {
-            ProviderInstaller.installIfNeeded(getApplicationContext());
-        } catch (GooglePlayServicesRepairableException e) {
-            e.printStackTrace();
-        } catch (GooglePlayServicesNotAvailableException e) {
-            e.printStackTrace();
-        }
+
+        // REMOVED ProviderInstaller: It was causing the SecurityException/Developer Error on SDK 34
+
         CountValue = 0;
-        //loadData();
-        Boolean showSplashScreen = false;
-        downloadStatus = (HashMap)SlideshowSharedPreferences.getPreferenceObjectDownloadStatus(SlideshowSplashScreen.this);
-        if(downloadStatus == null){
-            downloadStatus=new HashMap<>();
+        processedCount = 0;
+
+        downloadStatus = (HashMap) SlideshowSharedPreferences.getPreferenceObjectDownloadStatus(SlideshowSplashScreen.this);
+        if (downloadStatus == null) {
+            downloadStatus = new HashMap<>();
         }
-        Log.d("spl", "onCreate: downloadStatus "+downloadStatus.size());
-        if(downloadStatus.isEmpty()){
-            showSplashScreen = true;
-        }
-        Iterator myVeryOwnIterator = downloadStatus.keySet().iterator();
-        while(myVeryOwnIterator.hasNext()) {
-            String key=(String)myVeryOwnIterator.next();
-            Boolean value=(Boolean)downloadStatus.get(key);
-            Log.d("showSplashScreen", "showSplashScreen: key"+key+" value "+value);
-            if(!value){
-                showSplashScreen = true;
-                break;
+
+        boolean showSplashScreen = downloadStatus.isEmpty();
+        if (!showSplashScreen) {
+            for (Boolean value : downloadStatus.values()) {
+                if (!value) {
+                    showSplashScreen = true;
+                    break;
+                }
             }
         }
-        // if (!SlideshowSharedPreferences.GET_SAVE_IMAGES(SlideshowSplashScreen.this)) {
+
         if (showSplashScreen) {
-            Log.d("TAG", "1qa1 onCreate: GET_SAVE_IMAGES");
-//            loadJSONFromAsset(this);
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    new LoadJSON().doInBackground("");
-                }
-            });
+            new LoadJSON().execute("");
         } else {
-            Log.d("TAG", "1qa1 onCreate: GET_SAVE_IMAGES else");
-            finish();
-            String json_string = getIntent().getStringExtra("json_string");
-            startActivity(new Intent(SlideshowSplashScreen.this, SlideShowActivity.class)
-                    .putExtra("json_string", json_string)
-            );
-        }
-    }
-
-    public void loadJSONFromAsset(Context context) {
-        String json = null;
-        try {
-            InputStream is = context.getAssets().open("Local.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            json = new String(buffer, "UTF-8");
-            data = new Gson().fromJson(json, SlideshowJsonModel.class);
-            LoadImages();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-
+            proceedToNextActivity();
         }
     }
 
     private void LoadImages() {
-        cache = ImagesCache.getInstance();//Singleton instance handled in ImagesCache class.
+        cache = ImagesCache.getInstance();
         cache.initializeCache();
         boolean foundImages = false;
 
-        for (int slides = 0; slides < data.getSlides().size(); slides++) {
-            Log.d("TAG", "Slides: " + slides);
-            if (data.getSlides().get(slides).getBackgrounds() != null) {
-                for (int back = 0; back < data.getSlides().get(slides).getBackgrounds().size(); back++) {
-                    if (data.getSlides().get(slides).getBackgrounds().size() != 0 && data.getSlides().get(slides).getBackgrounds().get(back).getUrl() != null) {
-                        addImageInData(data.getSlides().get(slides).getBackgrounds().get(back).getUrl());
+        if (data == null || data.getSlides() == null) return;
+
+        // Collect all URLs first to know the total count
+        for (int i = 0; i < data.getSlides().size(); i++) {
+            if (data.getSlides().get(i).getBackgrounds() != null) {
+                for (int b = 0; b < data.getSlides().get(i).getBackgrounds().size(); b++) {
+                    String url = data.getSlides().get(i).getBackgrounds().get(b).getUrl();
+                    if (url != null && !url.isEmpty()) {
+                        addImageInCache(url);
                         foundImages = true;
                     }
                 }
             }
-            if (data.getSlides().get(slides).getLines() != null) {
-                for (int lines = 0; lines < data.getSlides().get(slides).getLines().size(); lines++) {
-                    Log.d("TAG", "Lines: " + lines);
-                    List<lineMedia> dImages = data.getSlides().get(slides).getLines().get(lines).getImages();
+            if (data.getSlides().get(i).getLines() != null) {
+                for (int l = 0; l < data.getSlides().get(i).getLines().size(); l++) {
+                    List<lineMedia> dImages = data.getSlides().get(i).getLines().get(l).getImages();
                     if (dImages != null) {
-                        for (int images = 0; images < dImages.size(); images++) {
-                            Log.d("TAG", "Images: " + images);
-                            if (dImages.get(images).getMediaAttr() != null) {
-                                addImageInData(dImages.get(images).getMediaFile().getImage().getUrl());
+                        for (int img = 0; img < dImages.size(); img++) {
+                            if (dImages.get(img).getMediaFile() != null && dImages.get(img).getMediaFile().getImage() != null) {
+                                String url = dImages.get(img).getMediaFile().getImage().getUrl();
+                                addImageInCache(url);
                                 foundImages = true;
-                                if ((dImages.get(images).getMediaAttr().getImageAudio() != null) && (!dImages.get(images).getMediaAttr().getImageAudio().getUrl().isEmpty())) {
-                                    addAudioInCache(dImages.get(images).getMediaAttr().getImageAudio().getUrl());
-                                }
                             }
                         }
                     }
                 }
             }
         }
-        if(!foundImages){
-            Log.d("TAG", "xdfre foundImages false IMAGES_SAVED: ");
-            SlideshowSharedPreferences.IMAGES_SAVED(SlideshowSplashScreen.this, true);
-            finish();
-            String json_string = getIntent().getStringExtra("json_string");
-            startActivity(new Intent(SlideshowSplashScreen.this, SlideShowActivity.class)
-                    .putExtra("json_string", json_string)
-            );
-        }
 
+        if (!foundImages) proceedToNextActivity();
     }
 
-    private void addAudioInCache(String url) {
-        Log.d("TAG", "addAudioInCache: "+url);
-        //downloadStatus.put(url, false);
-        //String proxyUrl = cacheProxy.getProxyUrl(url);
-        audioCountValue++;
-    }
-
-    private void addImageInData(String image) {
-        Log.d("TAG", "xdfre addImageInData: ");
-        if(downloadStatus.get(image) == null) {
-            downloadStatus.put(image, false);
+    private void addImageInCache(final String url) {
+        if (downloadStatus.get(url) == null) {
+            downloadStatus.put(url, false);
         }
-        //Bitmap bm = cache.getImageFromWarehouse(image);
         CountValue++;
-        imageView.setImageBitmap(null);
+
         Glide.with(this)
                 .asBitmap()
-                .load(image)
-                .into(new SimpleTarget<Bitmap>() {
+                .load(url)
+                .override(600, 600)
+                .timeout(15000)
+                .into(new CustomTarget<Bitmap>() {
                     @Override
                     public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
-                        //imageView.setImageBitmap(resource);
-                        downloadStatus.put(image, true);
+                        if (imageView != null) imageView.setImageBitmap(resource);
+                        cache.addBitmapToCache(url, resource);
+                        downloadStatus.put(url, true);
+                        checkCompletion();
+                    }
+
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) { }
+
+                    @Override
+                    public void onLoadFailed(@Nullable Drawable errorDrawable) {
+                        Log.e("GLIDE_ERROR", "Skipping dead link: " + url);
+                        // Mark as true (processed) so the app doesn't hang on dead links
+                        downloadStatus.put(url, true);
+                        checkCompletion();
                     }
                 });
-        //DownloadImageTask imgTask = new DownloadImageTask(SlideshowSplashScreen.this, cache, imageView, 600, 600, SlideshowSplashScreen.this);//Since you are using it from `Activity` call second Constructor.
-        //imgTask.execute(image);
+    }
+
+    private synchronized void checkCompletion() {
+        processedCount++;
+        // Save status periodically
+        SlideshowSharedPreferences.setPreferenceObject(SlideshowSplashScreen.this, downloadStatus, "downloadStatus");
+
+        // If all items in the current batch are processed, move forward
+        if (processedCount >= CountValue) {
+            runOnUiThread(() -> proceedToNextActivity());
+        }
+    }
+
+    private void proceedToNextActivity() {
+        SlideshowSharedPreferences.IMAGES_SAVED(SlideshowSplashScreen.this, true);
+        String json_string = new Gson().toJson(data);
+        Intent intent = new Intent(SlideshowSplashScreen.this, SlideShowActivity.class);
+        intent.putExtra("json_string", json_string);
+        startActivity(intent);
+        finish();
+    }
+
+    public void loadData() {
+        Retrofit retrofit = SlideshowRetrofitInstance.getRetrofitInstance();
+        SlideshowGetDataService service = retrofit.create(SlideshowGetDataService.class);
+        service.getAllJson().enqueue(new Callback<SlideshowJsonModel>() {
+            @Override
+            public void onResponse(Call<SlideshowJsonModel> call, Response<SlideshowJsonModel> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    data = response.body();
+                    SlideshowSharedPreferences.setPreferenceObject(SlideshowSplashScreen.this, data, "data");
+                    LoadImages();
+                } else {
+                    Log.e("API_ERROR", "Response failed");
+                }
+            }
+            @Override
+            public void onFailure(Call<SlideshowJsonModel> call, Throwable t) {
+                Log.e("API_ERROR", "Retrofit Failed: " + t.getMessage());
+            }
+        });
     }
 
     private class LoadJSON extends AsyncTask<String, String, String> {
-
         @Override
         protected String doInBackground(String... strings) {
             loadData();
             return "";
         }
     }
-
-    public void loadData() {
-        //Obtain an instance of Retrofit by calling the static method.
-        Retrofit retrofit = SlideshowRetrofitInstance.getRetrofitInstance();
-
-        SlideshowGetDataService json = retrofit.create(SlideshowGetDataService.class);
-        json.getAllJson().enqueue(new Callback<SlideshowJsonModel>() {
-            @Override
-            public void onResponse(Call<SlideshowJsonModel> call, Response<SlideshowJsonModel> response) {
-                if (response.isSuccessful()) {
-                    data = response.body();
-                    SlideshowSharedPreferences.setPreferenceObject(SlideshowSplashScreen.this, data, "data");
-                    LoadImages();
-                    // Before recursively calling splashscreen send downloadStatus to preferences
-                    SlideshowSharedPreferences.setPreferenceObject(SlideshowSplashScreen.this, downloadStatus, "downloadStatus");
-                    startActivity(new Intent(SlideshowSplashScreen.this, SlideshowSplashScreen.class));
-                }
-            }
-
-            @Override
-            public void onFailure(Call<SlideshowJsonModel> call, Throwable t) {
-
-            }
-        });
-    }
-
 }
